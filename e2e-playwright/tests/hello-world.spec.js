b37725fb9fc4dab2c2e42507a3bbe51820c258c4
Anselme Donato <anselme.donato2@gmail.com>
2023-10-20 16:29:26 +0300

    Test create new list

--- e2e-playwright/tests/hello-world.spec.js
+++ e2e-playwright/tests/hello-world.spec.js
@@ -11,3 +11,11 @@ test("Lists page linked in Main page", async ({ page }) => {
   await page.locator(`a >> text='Show lists'`).click();
   await expect(page.locator("h1")).toHaveText("Shared shopping lists - Lists");
 });
+
+test("Can create a task", async ({ page }) => {
+	await page.goto("/lists");
+	const listName = `My list: ${Math.random()}`;
+	await page.locator("input[type=text]").type(listName);
+	await page.locator("button[type=submit]").click();
+	await expect(page.locator(`li >> text='${listName}'`)).toHaveText(listName);
+  });
