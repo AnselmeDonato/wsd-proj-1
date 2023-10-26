3490900dddce7cd0a422c9188f7d11d238999d72
Anselme Donato <anselme.donato2@gmail.com>
2023-10-26 12:31:30 +0300

    Correction list items and items ordered

--- shopping-lists/services/itemService.js
+++ shopping-lists/services/itemService.js
@@ -1,10 +1,11 @@
 import { sql } from "../database/database.js";
 
 const getByListId = async (id) => {
-	return await sql`SELECT * FROM shopping_list_items WHERE shopping_list_id = ${ id } ORDER BY id DESC`; 
+	return await sql`SELECT * FROM shopping_list_items WHERE shopping_list_id = ${ id } ORDER BY name ASC`; 
 }; 
 
 const create = async (shopping_list_id, name) => {
+	console.log("create"); 
 	return await sql`INSERT INTO shopping_list_items (shopping_list_id, name) VALUES (${ shopping_list_id}, ${ name })`
 }; 
 
