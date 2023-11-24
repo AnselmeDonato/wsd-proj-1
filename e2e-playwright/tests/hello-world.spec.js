e33933d95ada6f7cdbfd8fd2e11dc2330ad2a120
Anselme Donato <anselme.donato2@gmail.com>
2023-11-24 20:45:00 +0200

    Fix of the old tests

--- e2e-playwright/tests/hello-world.spec.js
+++ e2e-playwright/tests/hello-world.spec.js
@@ -1,15 +1,14 @@
 const { test, expect } = require("@playwright/test");
 
-test("Main page has title 'Shared shopping lists'", async ({ page }) => {
+test("Main page shows statistics", async ({ page }) => {
   const response = await page.goto("/");
-  await expect(page).toHaveTitle("Shared shopping lists"); 
-  await expect(page.locator("h1")).toHaveText("Shared shopping lists"); 
+  await expect(page.locator("h5")).toHaveText(["Statistics:"]); 
 });
 
 test("Lists page linked in Main page", async ({ page }) => {
   const response = await page.goto("/");
-  await page.locator(`a >> text='Show lists'`).click();
-  await expect(page.locator("h1")).toHaveText("Shared shopping lists - Lists");
+  await page.locator(`a >> text='Lists'`).click();
+  await expect(page).toHaveURL("http://localhost:7777/lists"); 
 });
 
 test("Can create a task", async ({ page }) => {
@@ -18,4 +17,4 @@ test("Can create a task", async ({ page }) => {
 	await page.locator("input[type=text]").type(listName);
 	await page.locator("button[type=submit]").click();
 	await expect(page.locator(`li >> text='${listName}'`)).toHaveText(listName);
-  });
+});
