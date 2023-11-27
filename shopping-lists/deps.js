9ee9153280395dc53382b6cd7e38f6cdd9ecb7dd
Anselme Donato <anselme.donato2@gmail.com>
2023-11-27 12:50:03 +0200

    new-new postgres version

--- shopping-lists/deps.js
+++ shopping-lists/deps.js
@@ -1,4 +1,4 @@
 import { configure, renderFile } from "https://deno.land/x/eta@v2.2.0/mod.ts";
-import postgres from "https://deno.land/x/postgresjs@v3.4.3/mod.js";
+import postgres from "https://deno.land/x/postgresjs@v3.4.2/mod.js";
 
 export { configure, postgres, renderFile };
